f273bee4576bec6173e8766fca16a5905133c423
John-Witter <mrsolginski@gmail.com>
2021-06-30 18:30:45 -0400

    added saved_form, saved_routes, interests table for user defined tags

--- user-auth/app/api/saved_routes.py
+++ user-auth/app/api/saved_routes.py
@@ -4,14 +4,31 @@ from app.forms import SavedForm
 
 saved_routes = Blueprint('saved', __name__)
 
+#return all tags that a given user has selected
 @saved_routes.route('/<int:userId>', methods=["GET"])
 def get_saved(userId):
     saved = Saved.query.filter_by(userId == userId).all()
 
     return {"saved":[article.to_dict() for article in saved]}
 
+# adds a tag to the current user's selected tags
+@saved_routes.route('<int:userId', methods=["POST"])    
+def post_to_saved(userId):
+    pass
+
+# returns single saved article for the given article id
 @saved_routes.route('/articles/<int:id>', methods=["GET"])
 def get_saved(id):
     saved = Saved.query.filter_by(id == id).first()
 
     return {"saved": saved.to_dict()}
+
+# deletes an article from the user's saved articles list
+@saved_routes.route('/articles/<int:id>', methods=['DELETE'])
+def delete_from_saved(id):
+        article = Saved.query.filter_by(id == id).first()
+
+        db.session.delete(article)
+        db.session.commit()
+
+        return '', 200

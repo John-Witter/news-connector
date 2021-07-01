eafbb1974b27170939c9d0284eb7367b11cafce7
John-Witter <mrsolginski@gmail.com>
2021-06-30 20:11:00 -0400

    interest.py model and import into models init.py

--- user-auth/app/models/__init__.py
+++ user-auth/app/models/__init__.py
@@ -2,3 +2,4 @@ from .db import db
 from .user import User
 from.tag import Tag
 from .saved import Saved
+from .interest import Interest
---
+++ user-auth/app/models/interest.py
@@ -0,0 +1,8 @@
+from .db import db
+
+class Interest(db.Model):
+    __tablename__ = 'interests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    userId = db.Columne(db.Integer, db.ForeignKey('users.id'),nullable=False)

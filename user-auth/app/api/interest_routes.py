f357d6a9dc0012f08b688a9265a8a8e5e4ebdc77
John-Witter <mrsolginski@gmail.com>
2021-06-30 21:42:59 -0400

    working on interest_routes

--- user-auth/app/api/interest_routes.py
+++ user-auth/app/api/interest_routes.py
@@ -1,3 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 from app.models import db, Interest
 from app.forms import InterestForm
+
+interest_routes = Blueprint('interests', __name__)
+
+# return all of the user's user-created interests
+@interest_routes.route('/', methods=['GET'])
+def get_interests():
+    interests = Interest.query.all()
+    return {"interests": [titles.to_dict for titles in interests]}
+
+# add and interest to view within the newsfeed
+@interest_routes.route('/', methods=["POST"])
+def post_interests():
+    form = InterestForm()   
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_interest = InterestForm(
+            userId = form.data['userId'],
+            title = form.data['title']
+        )
+        db.session.add(new_interest)
+        db.commit()
+
+        return new_interest.to_dict()
+
+# update the interest with the given id
+@interest_routes.route('/<int:id>', methods=['PUT'])    
+def update_interest(id):
+    
--- user-auth/app/models/interest.py
+++ user-auth/app/models/interest.py
@@ -6,3 +6,10 @@ class Interest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False, unique=True)
     userId = db.Columne(db.Integer, db.ForeignKey('users.id'),nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'userId': self.userId
+        }

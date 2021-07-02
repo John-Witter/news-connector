6b6cffff718e49940882853f3b1f48c69de7b295
John-Witter <mrsolginski@gmail.com>
2021-07-02 13:23:49 -0400

    changed return state of NEW_INTEREST to have key of action.interest.title

--- user-auth/react-app/src/store/interests.js
+++ user-auth/react-app/src/store/interests.js
@@ -23,16 +23,18 @@ export const addInterest = (userId, title) => async (dispatch) => {
     if (res.ok) {
         const data = await res.json()
         console.log(data)
-        dispatch(addNewInterest(data)) }
+        dispatch(addNewInterest(data))
+    }
 }
 
 // reducer
 export default function InterestReducer(state={}, action) {
-    const newState={}
+    let newState={}
     switch(action.type) {
         case NEW_INTEREST:
-            newState[action.interest.id] = action.interest
-            return {...state, newState}
+            newState = {...state}
+            newState[action.interest.title] = action.interest
+            return newState
         default:
             return state
     }

93fdef60d35f55181d202a2d82808076cf3607bd
John-Witter <mrsolginski@gmail.com>
2021-07-07 12:00:21 -0400

    testing articles_routes interests and tags

--- user-auth/app/api/article_routes.py
+++ user-auth/app/api/article_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
+from flask_login import current_user
 import requests, json
-from app.models import db, Interest
+from app.models import db, Interest, Tag
 
 article_routes = Blueprint('articles', __name__)
 
@@ -9,4 +10,12 @@ def get_articles():
     res = requests.get(
         'https://newsapi.org/v2/everything?q=Apple&from=2021-06-26&sortBy=popularity&apiKey=13bc774f3bb545d8935600ca47e4cfcf')
     
-    return res.json()
+    interestsFromDB = Interest.query.filter_by(userId = current_user.id).all()
+    interests = [titles.to_dict() for titles in interestsFromDB]
+    print (interests)
+
+    tagsFromDB = Tag.query.all()
+    tags = [tags.to_dict() for tags in tagsFromDB]
+    print (tags)
+
+    return ''

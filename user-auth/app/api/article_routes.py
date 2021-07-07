cd06e0386df0c1f76851ba47ee3731f54eb400cc
John-Witter <mrsolginski@gmail.com>
2021-07-07 12:11:56 -0400

    article_routes has list oof titles !

--- user-auth/app/api/article_routes.py
+++ user-auth/app/api/article_routes.py
@@ -10,12 +10,16 @@ def get_articles():
     res = requests.get(
         'https://newsapi.org/v2/everything?q=Apple&from=2021-06-26&sortBy=popularity&apiKey=13bc774f3bb545d8935600ca47e4cfcf')
     
+    # get  titles from user's interests
     interestsFromDB = Interest.query.filter_by(userId = current_user.id).all()
-    interests = [titles.to_dict() for titles in interestsFromDB]
-    print (interests)
+    interests = [title.to_dict()['title'] for title in interestsFromDB]
 
+    # get titles from tags
     tagsFromDB = Tag.query.all()
-    tags = [tags.to_dict() for tags in tagsFromDB]
-    print (tags)
+    tags = [tag.to_dict()['title'] for tag in tagsFromDB]
+
+    # merge titles from tags and interests into one list
+    titles = tags + interests
+    print('!!!!!!titles:', titles)
 
     return ''

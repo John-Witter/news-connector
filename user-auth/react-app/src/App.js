292447133051542f139edec6ea988f90c60fa0cb
John-Witter <mrsolginski@gmail.com>
2021-07-10 18:20:22 -0400

    cleaning up styles

--- user-auth/react-app/src/App.js
+++ user-auth/react-app/src/App.js
@@ -5,11 +5,11 @@ import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
-import UsersList from "./components/UsersList";
 import User from "./components/User";
 import Content from "./components/content/Content";
 import Interests from "./components/interests/Interests";
 import Saved from "./components/saved/Saved";
+import Title from "./components/title/Title";
 import { authenticate } from "./store/session";
 
 function App() {
@@ -37,13 +37,11 @@ function App() {
         <Route path="/sign-up" exact={true}>
           <SignUpForm />
         </Route>
-        <ProtectedRoute path="/users" exact={true}>
-          <UsersList/>
-        </ProtectedRoute>
         <ProtectedRoute path="/users/:userId" exact={true}>
           <User />
         </ProtectedRoute>
         <ProtectedRoute path="/" exact={true} >
+          <Title />
           <Content />
           <Saved />
           <Interests />
---
+++ user-auth/react-app/src/components/title/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Title

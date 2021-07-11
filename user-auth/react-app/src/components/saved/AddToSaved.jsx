9d79af949b7f032154be0dd32adb16d06819c051
John-Witter <mrsolginski@gmail.com>
2021-07-10 21:59:30 -0400

    updated AddToSaved.jsx to take in correct info

--- user-auth/react-app/src/components/saved/AddToSaved.jsx
+++ user-auth/react-app/src/components/saved/AddToSaved.jsx
@@ -3,21 +3,23 @@ import { useDispatch, useSelector } from "react-redux";
 import '../content.css'
 import './saved.css'
 
-const AddToSaved = () => {
+const AddToSaved = ({ item }) => {
     const dispatch = useDispatch()
     const user = useSelector(state => state.session.user)
     let userId = user['id']
     const content = useSelector(state => state)
-    const [savedText, setSavedText] = useState('Add to Saved')
-
-    // if (user) {
-    //     userId = user['id']
-    // }
+    const [savedText, setSavedText] = useState('Add to Saved')    
 
     const handleAddToSaved = () => {
+        let {title, url, imageURL, description} = item
+        console.log('title:', title)
+        console.log('url:', url)
+        console.log('imageURL:', imageURL)
+        console.log('description:', description)
         // if savedText === 'Add to Saved' dispatch(postToSaved(userId, itemURL))
         // else dispatch(deleteFromSaved)
         console.log('CLIKED: ', savedText)
+        console.log('ITEM:', item)
 
         if (savedText === 'Add to Saved') {
         }

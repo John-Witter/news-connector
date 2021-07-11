8700f2ebca10a16b8e511d36024a9491052743cd
John-Witter <mrsolginski@gmail.com>
2021-07-11 18:29:47 -0400

    styled default content

--- user-auth/react-app/src/components/default/Default.jsx
+++ user-auth/react-app/src/components/default/Default.jsx
@@ -1,25 +1,27 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import { loadDefault } from '../../store/default'
+import '../content.css'
+import '../articles/articles.css'
 
 const Default = () => {
     const dispatch = useDispatch()
     const defaults = useSelector(state => state.default)
-console.log('defaults:', defaults)
+
     useEffect(() => {
         dispatch(loadDefault())
     }, [dispatch])
 
     return (
-        <div>
+        <div className='parent'>
             {defaults && Object.values(defaults).map(item => {
                 return (
-                    <div key={item.id}>
+                    <div key={item.id} className='content-container'>
                         <h2><a href={item.itemURL}>{item.title}</a></h2>
                         <a href={item.url}>
                             <img src={item.imageURL} alt={item.title} />
                         </a>
-                        {item.description && (<div>{item.description}</div>)}
+                        {item.description && (<div className='description'>{item.description}</div>)}
                     </div>
                 )
             })}

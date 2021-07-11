323deae3adc3e8d4b26f6cb7259a36e23a7bd472
John-Witter <mrsolginski@gmail.com>
2021-07-11 17:11:30 -0400

    do i need img url for defaults?

--- user-auth/react-app/src/components/default/Default.jsx
+++ user-auth/react-app/src/components/default/Default.jsx
@@ -5,14 +5,24 @@ import { loadDefault } from '../../store/default'
 const Default = () => {
     const dispatch = useDispatch()
     const defaults = useSelector(state => state.default)
-
+console.log('defaults:', defaults)
     useEffect(() => {
         dispatch(loadDefault())
     }, [dispatch])
 
     return (
         <div>
-            Default
+            {defaults && Object.values(defaults).map(item => {
+                return (
+                    <div key={item.id}>
+                        <h2><a href={item.url}>{item.title}</a></h2>
+                        <a href={item.url}>
+                            <img src={item.url} alt={item.title} />
+                        </a>
+                        {item.description && (<div>{item.description}</div>)}
+                    </div>
+                )
+            })}
         </div>
     )
 }

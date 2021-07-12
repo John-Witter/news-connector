2ed742d193790a06f8546ad963d804b31ebbd733
John-Witter <mrsolginski@gmail.com>
2021-07-11 21:32:39 -0400

    saved content is displayed

--- user-auth/react-app/src/components/saved/SavedList.jsx
+++ user-auth/react-app/src/components/saved/SavedList.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { loadSavedArticles } from '../../store/saved'
+import '../content.css'
+import '../articles/articles.css'
 
 const SavedList = () => {
     const dispatch = useDispatch()
     const user = useSelector(state => state.session.user)
+    const savedArticles = useSelector(state => state.saved)
 
     useEffect(() => {
         if(!user) return 
         dispatch(loadSavedArticles())
     }, [dispatch])
+
+    console.log('savedArticles:', savedArticles)
 
     if (!user) {
         return null
@@ -18,6 +23,17 @@ const SavedList = () => {
     return (
         <div className='saved'>
             <p>Saved</p>
+            {savedArticles && Object.values(savedArticles).map(item => {
+                return (
+                    <div key={item.id} className='content-container'>
+                        <h2><a href={item.itemURL}>{item.title}</a></h2>
+                        <a href={item.itemURL}>
+                            <img src={item.imageURL} alt={item.title} />
+                        </a>
+                        {item.description && (<div className='description'>{item.description}</div>)}
+                    </div>
+                )
+            })}
         </div>
     )
 }

e9586084d60ca619354c4e1ad2e841f9e119e24c
John-Witter <mrsolginski@gmail.com>
2021-07-11 22:19:13 -0400

    getting location by ip

--- user-auth/react-app/src/components/weather/Weather.jsx
+++ user-auth/react-app/src/components/weather/Weather.jsx
@@ -1,12 +1,24 @@
 import React, { useEffect, useState} from 'react'
 
 const Weather = () => {
-    const [lat, setLat] = useState([])
-    const [long, setLong] = useState([])
+    const [lat, setLat] = useState('')
+    const [long, setLong] = useState('')
+    const [city, setCity] = useState('')
 
-    ipLocation('', function (err, myLocation) {
-        console.dir(myLocation)
-    })
+    const getLocationByIP = async () => {
+        const res = await fetch('https://ipapi.co/json/')
+        const data = await res.json()
+        console.log('getLocationByIP data:', data)
+        setLat(data.latitude)
+        setLong(data.longitude)
+        setCity(data.city)
+    }
+
+    getLocationByIP()
+
+    // ipLocation('', function (err, myLocation) {
+    //     console.dir(myLocation)
+    // })
 
     console.log("Latitude is:", lat)
     console.log("Longitude is:", long)
@@ -15,6 +27,7 @@ const Weather = () => {
         <div>
             <h1>Long {long}</h1>
             <h1>Lat {lat}</h1>
+            <h1>City {city}</h1>
         </div>
     )
 }

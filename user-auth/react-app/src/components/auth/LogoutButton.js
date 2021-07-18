b00609c6637877e3ff0356d3248a1bea35fce825
John-Witter <mrsolginski@gmail.com>
2021-07-18 11:11:18 -0400

    styled logoutButton, navbar

--- user-auth/react-app/src/components/auth/LogoutButton.js
+++ user-auth/react-app/src/components/auth/LogoutButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
+import './logoutButton.css'
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
@@ -8,7 +9,7 @@ const LogoutButton = () => {
     await dispatch(logout());
   };
 
-  return <button onClick={onLogout}>Logout</button>;
+  return <button onClick={onLogout} className='logoutBtn'>Logout</button>;
 };
 
 export default LogoutButton;

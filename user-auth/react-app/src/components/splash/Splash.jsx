f6d7aa5fab5009e02e3d3ea0b17078b6ec40ae81
John-Witter <mrsolginski@gmail.com>
2021-07-23 09:53:28 -0400

    bg-img and tag line

--- user-auth/react-app/src/components/splash/Splash.jsx
+++ user-auth/react-app/src/components/splash/Splash.jsx
@@ -4,7 +4,7 @@ import './splash.css'
 const Splash = () => {
     return (
         <div className='mainImageContainer'>
-            
+            <p className="tagLine">Connect to Your Interests</p>
             <div className='mainImage'>
     
             </div>

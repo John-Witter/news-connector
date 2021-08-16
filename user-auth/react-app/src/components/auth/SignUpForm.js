14571aaff8ec037d8d6d7f5c1f2597b8ac33d1f7
John-Witter <mrsolginski@gmail.com>
2021-08-16 11:29:03 -0400

    signup-outermost-container, signup-form, signup labels htmlFor

--- user-auth/react-app/src/components/auth/SignUpForm.js
+++ user-auth/react-app/src/components/auth/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import './signUpForm.css'
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
@@ -39,46 +40,52 @@ const SignUpForm = () => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        <label>User Name</label>
-        <input
-          type="text"
-          name="username"
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type="text"
-          name="email"
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type="password"
-          name="repeat_password"
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className='signup-outermost-container'>
+      <form onSubmit={onSignUp} className='signup-form'>
+        <div className='name-input-container'>
+          <label htmlFor="name">User Name</label>
+          <input
+            type="text"
+            id="name"
+            name="username"
+            onChange={updateUsername}
+            value={username}
+          ></input>
+        </div>
+        <div className='email-input-container'>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            onChange={updateEmail}
+            value={email}
+          ></input>
+        </div>
+        <div className='password-input-container'>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={updatePassword}
+            value={password}
+          ></input>
+        </div>
+        <div className='repeat-password-input-container'>
+          <label htmlFor="repeat_password">Repeat Password</label>
+          <input
+            type="password"
+            id="repeat_password"
+            name="repeat_password"
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+            required={true}
+          ></input>
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
   );
 };
 

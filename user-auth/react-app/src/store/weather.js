5af37bffcded2e1113f82d83d73556d8d08a48d1
John-Witter <mrsolginski@gmail.com>
2021-11-14 19:20:39 -0500

    added defaultLocation in weather.js if users IP adress not found

--- user-auth/react-app/src/store/weather.js
+++ user-auth/react-app/src/store/weather.js
@@ -1,33 +1,47 @@
 // constants
-const GET_WEATHER = 'weather/GET_WEATHER'
+const GET_WEATHER = "weather/GET_WEATHER";
 
 // actions
 const loadWeather = (weather) => ({
     type: GET_WEATHER,
-    weather
-})
+    weather,
+});
 
 // thunks
 export const getWeather = () => async (dispatch) => {
-    const location = await fetch('https://ipapi.co/json/')
-    const locData = await location.json() 
+    try {
+        const location = await fetch("https://ipapi.co/json/");
+        const locData = await location.json();
 
-    const {city, latitude, longitude} = locData
-    const loc = city + '+' + latitude + '+' + longitude
-    const res = await fetch(`/api/weather/${loc}`)
+        const { city, latitude, longitude } = locData;
+        const loc = city + "+" + latitude + "+" + longitude;
+        const res = await fetch(`/api/weather/${loc}`);
 
-    if (res.ok) {
-        const data = await res.json()
-        dispatch(loadWeather(data))
-    } 
-}
+        if (res.ok) {
+            const data = await res.json();
+            dispatch(loadWeather(data));
+        }
+    } catch (error) {
+        // if ip location is not found
+        // set location to Miami
+
+        const defaultLocation = 'Miami' + "+" + 25.7381 + "+" + -80.312;
+        const defaultWeatherRes = await fetch(
+            `/api/weather/${defaultLocation}`
+        );
+
+        const defaultWeather = await defaultWeatherRes.json();
+
+        dispatch(loadWeather(defaultWeather));
+    }
+};
 
 // reducer
-export default function WeatherReducer (state={}, action) {    
-    switch(action.type) {
+export default function WeatherReducer(state = {}, action) {
+    switch (action.type) {
         case GET_WEATHER:
-            return action.weather
+            return action.weather;
         default:
-            return state
+            return state;
     }
 }

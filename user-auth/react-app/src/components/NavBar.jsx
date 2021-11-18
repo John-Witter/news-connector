e84cd4c30f433519f9feda460af0465e0b4287e0
John-Witter <mrsolginski@gmail.com>
2021-11-18 14:45:30 -0500

    ReactGA Event on Logout button

--- user-auth/react-app/src/components/NavBar.jsx
+++ user-auth/react-app/src/components/NavBar.jsx
@@ -1,54 +1,100 @@
-import React from 'react';
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
-import { login } from '../store/session';
-import { Event } from './Google Analytics/GoogleAnalytics';
-import './navbar.css'
-import './content.css'
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
+import { login } from "../store/session";
+import { Event } from "./Google Analytics/GoogleAnalytics";
+import "./navbar.css";
+import "./content.css";
 
 const NavBar = () => {
-  const user = useSelector(state => state.session.user)
-  const dispatch = useDispatch()
-  
-  const demoUser = async (e) => {
-    e.preventDefault()
-    const email = 'demo@aa.io'
-    const password = 'password'
-    await dispatch(login(email, password))
-    window.location.reload()
-  }
+    const user = useSelector((state) => state.session.user);
+    const dispatch = useDispatch();
 
-  // remove navbar once user logs in
-  // logout button will be rendered with title
-  // if (user) return (null)
+    const demoUser = async (e) => {
+        e.preventDefault();
+        const email = "demo@aa.io";
+        const password = "password";
+        await dispatch(login(email, password));
+        window.location.reload();
+    };
 
-  return (
-    <nav className='navContainer'>
-      <ul className='navbar'>
-        {!user && <li>
-          <NavLink className='authLink' to="/login" exact={true} activeClassName="active" onClick={() => Event("LOGIN LINK", "Link to login clicked", "NAV_BAR")}>
-            Login
-          </NavLink>
-        </li>}
-       {!user && <li>
-          <NavLink className='authLink' to="/sign-up" exact={true} activeClassName="active" onClick={() => Event("SIGNUP LINK", "Link to signup clicked", "NAV_BAR")}>
-            Sign Up
-          </NavLink>
-        </li>}
-        {user && <li>
-          <LogoutButton />
-        </li>}
-        {!user && <li className='demoBtn' onClick={e => {
-            Event("DEMO LINK", "Link to demo clicked", "NAV_BAR");
-            demoUser(e);
-        }}>
-          Demo User
-        </li>
-        }
-      </ul>
-    </nav>
-  );
-}
+    // remove navbar once user logs in
+    // logout button will be rendered with title
+    // if (user) return (null)
+
+    return (
+        <nav className="navContainer">
+            <ul className="navbar">
+                {!user && (
+                    <li>
+                        <NavLink
+                            className="authLink"
+                            to="/login"
+                            exact={true}
+                            activeClassName="active"
+                            onClick={() =>
+                                Event(
+                                    "LOGIN LINK",
+                                    "Link to login clicked",
+                                    "NAV_BAR"
+                                )
+                            }
+                        >
+                            Login
+                        </NavLink>
+                    </li>
+                )}
+                {!user && (
+                    <li>
+                        <NavLink
+                            className="authLink"
+                            to="/sign-up"
+                            exact={true}
+                            activeClassName="active"
+                            onClick={() =>
+                                Event(
+                                    "SIGNUP LINK",
+                                    "Link to signup clicked",
+                                    "NAV_BAR"
+                                )
+                            }
+                        >
+                            Sign Up
+                        </NavLink>
+                    </li>
+                )}
+                {user && (
+                    <li>
+                        <LogoutButton
+                            onClick={() =>
+                                Event(
+                                    "LOGOUT LINK",
+                                    "Link to logout clicked",
+                                    "NAV_BAR"
+                                )
+                            }
+                        />
+                    </li>
+                )}
+                {!user && (
+                    <li
+                        className="demoBtn"
+                        onClick={(e) => {
+                            Event(
+                                "DEMO LINK",
+                                "Link to demo clicked",
+                                "NAV_BAR"
+                            );
+                            demoUser(e);
+                        }}
+                    >
+                        Demo User
+                    </li>
+                )}
+            </ul>
+        </nav>
+    );
+};
 
 export default NavBar;
